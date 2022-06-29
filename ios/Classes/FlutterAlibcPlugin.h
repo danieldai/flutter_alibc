6c4b648c64e5e277f7a8a58c0fe99a590f771412
Daniel Dai <daeming@gmail.com>
2022-06-29 09:35:21 +0800

    wip

--- ios/Classes/FlutterAlibcPlugin.h
+++ ios/Classes/FlutterAlibcPlugin.h
@@ -1,7 +1,4 @@
 #import <Flutter/Flutter.h>
-#import <AlibcTradeSDK/AlibcTradeSDK.h>
-#import <AlibabaAuthEntrance/ALBBSDK.h>
-#import <AlibabaAuthEntrance/ALBBCompatibleSession.h>
-#import <WindVane/WindVane.h>
+
 @interface FlutterAlibcPlugin : NSObject<FlutterPlugin>
 @end
